ebda96bb870dfef17590956ff21d5be9aa9860e2
Daniel Parkin <danielparkin1@gmail.com>
2018-10-31 18:02:36 +0000

    array with 2 in it returns 2

--- test/evens-array.test.js
+++ test/evens-array.test.js
@@ -5,4 +5,12 @@ describe('evens array', () => {
   it('returns empty array when given empty array', () => {
     expect(filterEvensFrom([])).to.eql([])
   });
+
+  it('returns empty array when given array [1]', () => {
+    expect(filterEvensFrom([1])).to.eql([])
+  });
+
+  it('returns array [2] when given array [1, 2]', () => {
+    expect(filterEvensFrom([1, 2])).to.eql([2])
+  });
 });
